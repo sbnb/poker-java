3546c8ff1789cb61c03c852cedabc7055ce37bdf
Sean Noble <sean.noble.72@gmail.com>
2014-01-28 09:04:45 +0100

    Add src for Poker class

---
+++ src/main/java/in/webplay/poker/Poker.java
@@ -0,0 +1,13 @@
+package in.webplay.poker;
+
+public class Poker {
+    private final String name;
+
+    public Poker(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
